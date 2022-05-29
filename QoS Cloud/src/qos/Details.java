b100858ee517ec5eec4975e1aa5bcacfa4d1f92d
moreshkumar <58771774+moreshkumar@users.noreply.github.com>
2022-05-30 01:09:18 +0530

    Add files via upload

---
+++ QoS Cloud/src/qos/Details.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qos;
+import java.util.ArrayList;
+import java.util.List;
+import org.cloudbus.cloudsim.Vm;
+/**
+ *
+ * @author admin
+ */
+public class Details 
+{
+    static List<Vm> vmlist=new ArrayList<Vm>();
+    static ArrayList vmLt=new ArrayList();
+    static ArrayList taskLt=new ArrayList();
+    
+    static ArrayList selT[];
+    
+    static ArrayList Mp=new ArrayList();
+    static ArrayList Rp=new ArrayList();
+}
